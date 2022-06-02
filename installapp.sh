9e75b47ead99aac1b3e1b1b5e7a38f0e7a439b93
Aqeel Ijaz <103575370+aqeeleurus@users.noreply.github.com>
2022-06-02 18:39:40 +0500

    Update installapp.sh

--- installapp.sh
+++ installapp.sh
@@ -5,4 +5,5 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3 -y
+sudo apt install python3-pip
 pip3 install -r requirements.txt
